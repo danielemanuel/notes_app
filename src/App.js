0ebe4100680f880ad68fec213013d6fd2d8778b2
danielemanuel <daniel.costea@vkernel.ro>
2017-10-05 19:23:47 +0100

    * adding onChange event to inputs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import './App.css';
 import Header from './components/header';
 
 class App extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      title: '',
+      content: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="app">
@@ -13,10 +28,22 @@ class App extends Component {
       <div className= 'container'>
         <section className= 'add-item'>
           <form>
-            <input className='title' type='text' name='title' placeholder='Insert title...' />
+            <input 
+              className='title' 
+              type='text' 
+              name='title'
+              placeholder='Insert title...'
+              onChange={this.handleChange}
+              value={this.state.title} />
             <br />
             <br />
-            <input  className='content' type='text' name='content' placeholder='Insert content...' />
+            <input  
+              className='content' 
+              type='text' 
+              name='content' 
+              placeholder='Insert content...'
+              onChange={this.handleChange} 
+              value={this.state.content}/>
             <br />
             <br />
             <button> Add Note</button>
