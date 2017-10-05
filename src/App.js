f5667ca1a76308b66071a73912b9a99bb01bba2a
danielemanuel <daniel.costea@vkernel.ro>
2017-10-05 18:59:11 +0100

    * styled the form with css

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
        <header>
           <div className='wrapper'>
             <h1>Notes App  </h1>
           </div>
        </header>
-      <div className= 'constainer'>
+      <div className= 'container'>
         <section className= 'add-item'>
           <form>
-            <input type='text' name='title' placeholder='Insert title...' />
-            <input type='text' name='content' placeholder='Insert content...' />
+            <input className='title' type='text' name='title' placeholder='Insert title...' />
+            <br />
+            <br />
+            <input  className='content' type='text' name='content' placeholder='Insert content...' />
+            <br />
+            <br />
             <button> Add Note</button>
           </form>
           <section className='display-item'>
             <div className='wrapper'>
               <ul>
-              
+
 
               </ul>
 
