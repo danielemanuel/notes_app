fdc81d0bdc28849fc86fdf9864c8d03c9840a11d
danielemanuel <daniel.costea@vkernel.ro>
2017-10-05 18:39:59 +0100

    * firebase.js file created * inport firebase module in my app

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyCB8w1stWQvI60v5VtTlUnMwCe41Y6Xd-M",
+    authDomain: "notes-app-74c0d.firebaseapp.com",
+    databaseURL: "https://notes-app-74c0d.firebaseio.com",
+    projectId: "notes-app-74c0d",
+    storageBucket: "",
+    messagingSenderId: "1020529161734"
+  };
+  firebase.initializeApp(config);
+  export default firebase;
